712c27cc0ef020d1326c7526019a8b02dd766d5a
Love <loveadeniran1@gmail.com>
2022-09-28 16:05:31 +0100

    i made the delete button work perfectly

--- calc.js
+++ calc.js
@@ -1,29 +1,27 @@
 
 // all numbers buttons that displays to screen
 disp.value =""
-rem =[]
 const displayToScreen=(e)=>{
     disp.value += e
-    rem.push(disp.value)
 }
 
 //  the Equal button
 const equalTo=()=>{
    answer = "="+ eval(disp.value)
    disp.value += answer;
-   rem.push(disp.value)
+
 }
 
 
 
-// The delete button is not working perfectly
+// The delete button 
 const deleteBtn=()=>{
     if(disp.value===''){
         console.log('field is empty')
     }
     else{
-        delet = rem.pop()
-        disp.value= delet
+        
+        disp.value= disp.value.slice(0,-1)
         // rem.splice(0,-1)
         // console.log(rem)
     }
